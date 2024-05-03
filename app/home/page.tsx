602e10a415d0ecc2b65ead3ff822fc041f6b1a6c
JohnEsleyer <ralphjohnpolicarpio@outlook.com>
2024-05-03 13:24:15 +0800

    added implementations for adding collections and code snippets

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ProtectedPage from "../templates/protectedpage";
 import { testCollections, testSnippets } from "./testdata";
 import CodeBlock from "./codeblock";
-
+import Loading from "/public/loading.svg";
+import Image from "next/image";
 
 interface Collection {
     id: number;
@@ -13,7 +14,7 @@ interface Collection {
 
 }
 
-interface Snippet{
+interface Snippet {
     id: number;
     title: string;
     code: string;
@@ -23,139 +24,219 @@ interface Snippet{
 
 export default function Home() {
 
+    const scrollableDiv = useRef<HTMLDivElement>(null);
+
     const [collections, setCollections] = useState<Collection[]>(testCollections)
     const [snippets, setSnippets] = useState<Snippet[]>(testSnippets);
     const [orientation, setOrientation] = useState<string>('');
     const [activeCollection, setActiveCollection] = useState<Collection | null>(null);
 
+    // Loading indicator states
+    const [loadingAddCollection, setLoadingAddCollection] = useState(false);
+    const [loadingAddSnippet, setLoadingAddSnippet] = useState(false);
+
+
     useEffect(() => {
         const checkOrientation = () => {
             if (window.matchMedia("(orientation: portrait)").matches) {
-              setOrientation('Portrait');
+                setOrientation('Portrait');
             } else if (window.matchMedia("(orientation: landscape)").matches) {
-              setOrientation('Landscape');
+                setOrientation('Landscape');
             }
-          };
+        };
 
-          checkOrientation();
+        checkOrientation();
 
-          const resizeListener = () => {
+        const resizeListener = () => {
             checkOrientation();
-          };
-      
+        };
 
 
-            // Set the first collection as the default active collection
+
+        // Set the first collection as the default active collection
         setActiveCollection(collections[0]);
 
-          window.addEventListener('resize', resizeListener);
-      
-          return () => {
+        window.addEventListener('resize', resizeListener);
+
+        return () => {
             window.removeEventListener('resize', resizeListener);
-          };
-          
-         
-         
+        };
+
+
+
     }, []);
+
+    const scrollToBottom = () => {
+        if (scrollableDiv.current){
+            scrollableDiv.current.scrollTop = scrollableDiv.current.scrollHeight;
+        }
+    }
+
+    const handleAddCollection = async () => {
+        setLoadingAddCollection(true);
     
- 
-    
+        
+        setCollections([...collections, {
+            id: collections.length + 1,
+            title: "Collection " + collections.length + 1,
+            description: "Description"
+        }]);
+
+  
+        setTimeout(() => {
+            setLoadingAddCollection(false);
+        }, 1000);
+    };
+
+    const handleAddSnippet = async () => {
+        
+        setLoadingAddSnippet(true);
+        
+        setSnippets([...snippets, {
+            id: snippets.length + 1,
+            title: "Snippet " + snippets.length + 1,
+            code: ``,
+            language: "Python",
+            description: "Description"
+        }]);
+
+  
+        setTimeout(() => {
+            setLoadingAddSnippet(false);
+            scrollToBottom();
+        }, 1000);
+        
+    };
+
 
     return (
         <ProtectedPage>
             <div className="bg-gray-200 text-black h-screen flex font-sans">
                 <div className={`${orientation == "Portrait" ? 'hidden' : ''} w-64 h-full flex flex-col`}>
                     <div className="shadow">
-                    <p className="text-2xl font-bold p-2 flex justify-center">CodePocket</p>
-                    <div className="pt-4 flex flex-col">
-                        <button className="p-1">
-                        <p className="flex items-center pl-2 "> 
-                        <span className="material-symbols-outlined">
+                        <p className="text-2xl font-bold p-2 flex justify-center">CodePocket</p>
+                        <div className="pt-4 pb-2 flex flex-col">
+                            {/* // Search */}
+                            <button className="p-1 hover:bg-gray-400">
+                                <p className="flex items-center pl-2 ">
+                                    <span className="material-symbols-outlined">
                                         search
                                     </span>Search
-                        </p>
-                        </button>
-                        <button className="p-1">
-                        <p className="flex items-center pl-2 "> 
-                        <span className="material-symbols-outlined">
+                                </p>
+                            </button>
+                            {/* // Settings */}
+                            <button className="p-1 hover:bg-gray-400">
+                                <p className="flex items-center pl-2 ">
+                                    <span className="material-symbols-outlined">
                                         settings
                                     </span>Settings
-                        </p>
-                        </button>
-                        <button className="p-1">
-                        <p className="flex items-center pl-2"> 
-                        <span className="material-symbols-outlined">
+                                </p>
+                            </button>
+                            {/* // Signout */}
+                            <button className="p-1 hover:bg-gray-400">
+                                <p className="flex items-center pl-2">
+                                    <span className="material-symbols-outlined">
                                         logout
-                        </span>Sign out
-                        </p>
-                        </button>
-                         {/* // Add a collection Button */}
-                         <button className="flex-initial pl-2" >
-                            <span className="flex items-center">
-                                <span className="material-symbols-outlined">
-                                    add
+                                    </span>Sign out
+                                </p>
+                            </button>
+                            {/* // Add a collection Button */}
+                            <button 
+                            className="flex-initial pl-2 hover:bg-gray-400" 
+                            onClick={handleAddCollection}
+                            >
+                                <span className="flex items-center">
+                                    <span className="material-symbols-outlined">
+                                        add
+                                    </span>
+                                    Add a collection
+                                    {loadingAddCollection && <span className="pl-2">
+                                        <Image 
+                                            src={Loading}
+                                            alt={''}
+                                            width={30}
+                                            height={30}
+                                        />
+                                    </span>}
                                 </span>
-                                Add a collection
-                            </span>
-                        </button>
-                        {/* // End of Add a collection Button */}
+                            </button>
+                        </div>
+
                     </div>
-                   
-                    </div>
-                   
-                    <div className="flex flex flex-col overflow-y-auto pl-2">
+
+                    <div className="flex flex-col overflow-y-auto pl-2">
                         {/* <p className="flex-initial">Collections</p> */}
                         {/* // Collections */}
                         {collections.map((value, index) => (
-                            <div className="text-xl pl-2 hover:bg-gray-800 hover:text-white hover:rounded">
-                                <button>
+                            <button 
+                            className="flex justify-start"
+                            onClick={() => {
+                                setActiveCollection(value);
+                            }}
+                            >
+                                <div className="w-full flex text-xl pl-2 hover:bg-gray-800 hover:text-white hover:rounded">
                                     <p>{value.title}</p>
-                                </button>
-                            </div>
+                                </div>
+                            </button>
+
                         ))}
 
-                       
+
                     </div>
                     {/* // End of collections */}
                 </div>
                 {/* // Code Snippets Section */}
-                <div className="flex-1 overflow-y-auto">
-                        <div className=" p-2 shadow">
-                        <p className="text-2xl font-bold">{activeCollection?.title}</p>
-                        <button>
-                        <div className="flex w-44 flex items-center">
-                            
-                            <span className="text-2xl material-symbols-outlined">
-                                        add
-                            </span>
-                            <span className="flex items-center">New code snippet</span>
-                        
-                        </div>
+                <div className="flex-1 flex flex-col">
+                    <div className="p-2 shadow">
+                        <p className="text-2xl font-bold flex"><span>{activeCollection?.title}</span>{loadingAddSnippet && <span className="pl-2">
+                                        <Image 
+                                            src={Loading}
+                                            alt={''}
+                                            width={30}
+                                            height={30}
+                                        />
+                                    </span>} </p> 
+                        {/* // New code snippet */}
+                        <div className="flex space-x-4">
+                        <button 
+                            className="hover:bg-gray-400 rounded"
+                            onClick={handleAddSnippet}
+                            >
+                            <div className="flex w-44 flex items-center">
+
+                                <span className="text-2xl material-symbols-outlined">
+                                    add
+                                </span>
+                                <span className="flex items-center ">New code snippet </span>
+
+                            </div>
                         </button>
-                        <button>
-                        <div className="flex w-44 flex items-center">
-                            
-                            <span className="text-2xl material-symbols-outlined">
-                                        share
-                            </span>
-                            <span className="flex items-center">Share</span>
-                        
-                        </div>
+                        {/* // Share collection */}
+                        <button className="hover:bg-gray-400 rounded ">
+                            <div className="flex w-20 flex items-center">
+
+                                <span className="text-2xl material-symbols-outlined">
+                                    share
+                                </span>
+                                <span className="flex items-center">Share</span>
+
+                            </div>
                         </button>
                         </div>
-                        
-                        <div className="flex flex-wrap">
+                    </div>
+
+                    <div className="flex-1 flex flex-wrap overflow-y-auto" ref={scrollableDiv}>
                         {snippets.map((value, index) => (
-                            <div className="m-2 h-80 ">
+                            <div className="m-2 h-80 fade-in visible">
                                 <p className="text-2xl">{value.title}</p>
                                 <p className="">{value.description}</p>
                                 <div className="h-60 overflow-x-hidden rounded-2xl ">
-                                <CodeBlock code={value.code}/>
+                                    <CodeBlock code={value.code} />
                                 </div>
-                                
+
                             </div>
                         ))}
-                        </div>
+                    </div>
                 </div>
                 {/* // End Code Snippets Section */}
             </div>
