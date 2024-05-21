33cb7935badf725268dc47a948f7c3b865f747f6
JohnEsleyer <ralphjohnpolicarpio@outlook.com>
2024-05-22 06:07:23 +0800

    update ProtectedPage template, now redirects client to root if not authorized

--- app/templates/protectedpage.tsx
+++ app/templates/protectedpage.tsx
@@ -3,18 +3,27 @@
 import { Session } from "@supabase/supabase-js";
 import supabase from "../utils/supabase";
 import React, { Children, ReactNode, useEffect, useState } from 'react';
+import { useRouter } from "next/navigation";
+
 
 interface Props {
     children: ReactNode;
 }
 
 export default function ProtectedPage({ children }: Props) {
+    const router = useRouter();
+
     const [session, setSession] = useState<Session | null>(null);
     
     useEffect(() => {
+        
         async function supabaseExecution() {
             supabase.auth.getSession().then(({ data: { session } }) => {
-                setSession(session)
+                if (session){
+                    setSession(session)
+                }else{
+                    router.push('/');
+                }
               })
         }
         supabaseExecution();
